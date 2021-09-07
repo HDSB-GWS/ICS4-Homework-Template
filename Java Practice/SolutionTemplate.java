2ae79ebf4e9cd6b513507c3b96e478ee852e0115
Chris Brooks-Prenger <brooksprench@hdsb.ca>
2021-09-07 13:00:42 -0400

    Initial commit

---
+++ Java Practice/SolutionTemplate.java
@@ -0,0 +1,15 @@
+/*-----------------------------------------------------------------------------
+Practice Name:       
+Question Number:     
+
+TODO: enter question details here
+
+Author:
+Date and Time Completed:
+-----------------------------------------------------------------------------*/
+
+class SolutionTemplate {
+  public static void main(String[] args) {
+    System.out.println("hello world");
+  }
+}
